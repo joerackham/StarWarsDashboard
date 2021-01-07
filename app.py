63ea69975e28d5abd3d70b5efb27bc2f817133ee
Joe Rackham <joerackham@hotmail.co.uk>
2021-01-07 20:31:11 +0000

    Add monologue

--- app.py
+++ app.py
@@ -16,13 +16,23 @@ def load_data(film_num):
     data = pd.read_csv('./Episode' + str(film_num) + '.txt', sep=" ", header=None)
     return data
 
+def write_wordcloud(data):
+    wordcloud = WordCloud(stopwords=STOPWORDS, background_color='white', height=640, width=800).generate(data)
+    fig, ax = plt.subplots()
+    plt.imshow(wordcloud)
+    plt.xticks([])
+    plt.yticks([])
+    st.pyplot(fig)
+
 st.title("Analysing who says what in the first three Star Wars films 🚀")
 st.sidebar.title("Who says what in the first three Star Wars films?")
-st.sidebar.write("By Joe Rackham")
+st.sidebar.markdown("By [Joe Rackham](https://joealexanderrackham.co.uk/)")
 flim_choice = st.sidebar.multiselect('Which films do you want to show data for?', FILMS)
 
 if(len(flim_choice) != 0):
     data = [(load_data(FILMS.index(film) + 4), film) for film in flim_choice]
+
+    min_lines = st.sidebar.slider("Minimum number of lines to be considered", 10, 100)
 
     # Lines by speaker
     st.markdown("### How many lines does each character say?")
@@ -33,7 +43,7 @@ if(len(flim_choice) != 0):
         lines = pd.DataFrame({'Character': lines.index, 'Lines': lines.values, 'Film': f})
         lines_by_speaker = lines if lines_by_speaker is None else lines_by_speaker.append(lines)
 
-    lines_by_speaker_trimmed = lines_by_speaker[lines_by_speaker['Lines'] > 10]
+    lines_by_speaker_trimmed = lines_by_speaker[lines_by_speaker['Lines'] > min_lines]
     fig = px.bar(lines_by_speaker_trimmed, height=750, width=800, x='Character', y='Lines', color='Film')
     st.write(fig)
 
@@ -50,21 +60,10 @@ if(len(flim_choice) != 0):
         all_lines += ' '.join(d[2])
         char_lines += ' '.join(d_char[2])
 
-    wordcloud = WordCloud(stopwords=STOPWORDS, background_color='white', height=640, width=800).generate(all_lines)
-    fig, ax = plt.subplots()
-    plt.imshow(wordcloud)
-    plt.xticks([])
-    plt.yticks([])
-    st.pyplot(fig)
+    write_wordcloud(all_lines)
 
     st.markdown("### And just the character you're focussing on")
-    wordcloud = WordCloud(stopwords=STOPWORDS, background_color='white', height=640, width=800).generate(char_lines)
-    fig, ax = plt.subplots()
-    plt.imshow(wordcloud)
-    plt.xticks([])
-    plt.yticks([])
-    st.pyplot(fig)
-
+    write_wordcloud(char_lines)
     
     # How positive
     st.markdown('### How positive are the characters?')
@@ -72,19 +71,31 @@ if(len(flim_choice) != 0):
     sid = SentimentIntensityAnalyzer()
     
     char_sent = []
+    char_line_length = []
     for c in characters:
         sentiment = 0.0
+        length = 0
         num_lines = 0
         for d, f in data:
             d_char = d[d[1] == c]
             for line in d_char[2]:
                 i = sid.polarity_scores(line)
                 sentiment += i['compound']
+                length += len(line.split())
                 num_lines += 1
 
         char_sent.append(sentiment / num_lines)
+        char_line_length.append(length / num_lines)
     
     sentiment_data = pd.DataFrame({'Character': characters, 'Sentiment': char_sent})
     fig = px.bar(sentiment_data, height=750, width=800, x='Character', y='Sentiment', color='Sentiment')
     st.write(fig)
 
+
+    # Longest lines
+    st.markdown("### Whose the monologuer?")
+    st.markdown("Which character has the longest lines?")
+
+    sentiment_data = pd.DataFrame({'Character': characters, 'Average Line Length': char_line_length})
+    fig = px.bar(sentiment_data, height=750, width=800, x='Character', y='Average Line Length')
+    st.write(fig)
